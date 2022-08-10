a82981a90ddd338bdbf6dccd28c7ada8624d8b17
palajo <palajoleg@gmail.com>
2022-08-10 04:05:27 +0300

    [update] added contacts to footer, added footer for mobile

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
@@ -7,7 +7,8 @@ import Script from 'next/script';
 import { Col, Container, Row } from 'react-bootstrap';
 
 import LogoWhite from '../../images/logo-white.svg';
-
+import PhoneIcon from '../../images/icons/phone.svg';
+import EmailIcon from '../../images/icons/email.svg';
 
 function Footer() {
   const router = useRouter();
@@ -27,15 +28,57 @@ function Footer() {
 
   return (
     <>
-      <footer className="d-none d-xl-block">
+      <footer>
         <Container className="container">
           <Row className="row">
             <Col xs={12}>
-              <Link href="/">
-                <a>
-                  <Image src={LogoWhite} alt="Dion - газові водонагрівачі"/>
-                </a>
-              </Link>
+              <Row className="justify-content-between align-items-lg-center gy-4">
+                <Col xs="auto">
+                  <Link href="/">
+                    <a>
+                      <Image src={LogoWhite} alt="Dion - газові водонагрівачі"/>
+                    </a>
+                  </Link>
+                </Col>
+                <Col xs="auto">
+                  <Row className="gx-lg-5 gy-4">
+                    <Col xs="auto">
+                      <div className="contact-block">
+                        <div className="block-title">
+                          E-mail
+                        </div>
+                        <div className="block-content">
+                          <div className="block-icon">
+                            <Image src={EmailIcon} layout="fixed" width={18} height={18} alt="Phone Icon" />
+                          </div>
+                          <div className="block-description">
+                            <Link href="mailto:sales@dion.lviv.ua">
+                              sales@dion.lviv.ua
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </Col>
+                    <Col xs="auto">
+                      <div className="contact-block">
+                        <div className="block-title">
+                          Телефон
+                        </div>
+                        <div className="block-content">
+                          <div className="block-icon">
+                            <Image src={PhoneIcon} layout="fixed" width={18} height={18} alt="Phone Icon" />
+                          </div>
+                          <div className="block-description">
+                            <Link href="tel:+38 (050) 330 36 10">
+                              +38 (050) 330 36 10
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
             </Col>
             <Col xs={12}>
               <hr />
